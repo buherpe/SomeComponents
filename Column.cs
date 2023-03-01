b63b40b0154434a2f172a203571138359a8947ba
Anatoly Filyuk <buherpet.38@gmail.com>
2023-03-01 10:57:27 +0500

    сырой датагрид перенесен в RCL без вьюх

---
+++ Column.cs
@@ -0,0 +1,11 @@
+﻿namespace SomeComponents
+{
+    public class Column
+    {
+        public string PropertyName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string SortingKey { get; set; }
+    }
+}
---
+++ LoadDataArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace SomeComponents
+{
+    public class LoadDataArgs
+    {
+        public string SearchText { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public string SortingColumn { get; set; }
+
+        public bool SortingDescending { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
